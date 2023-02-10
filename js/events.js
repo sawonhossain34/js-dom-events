8fe89a3711da9fe1ddf88543d7dfe1248ac24cff
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-10 16:15:45 +0600

    addEventlisener  backgroundColor 7 system apply

--- js/events.js
+++ js/events.js
@@ -13,3 +13,25 @@ const makeBlueButton =document.getElementById('make-blue');
     function makeBlue(){
         document.body.style.backgroundColor = 'blue';
     }
+    // option 3 another
+    const makePurpleButton = document.getElementById('make-purple');
+      makePurpleButton.onclick = function makePurple(){
+        document.body.style.backgroundColor='purple';
+      }
+    //   option 4
+      const pinkButton = document.getElementById('make-pink');
+       pinkButton.addEventListener('click',makePink)
+       
+       function makePink(){
+        document.body.style.backgroundColor = 'pink';
+       }
+    //    another 4 option
+    const greenButton = document.getElementById('make-green');
+      greenButton.addEventListener('click',function makeGreen(){
+        document.body.style.background = 'green';
+      })
+
+    //   Final 4 option
+    document.getElementById('make-goldenrod').addEventListener('click',function(){
+        document.body.style.backgroundColor= 'goldenrod';
+      })
